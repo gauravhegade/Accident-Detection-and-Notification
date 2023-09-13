647a28997b30e5312963254ace5370a275a98c90
Gaurav Hegade <neph005@protonmail.com>
2023-09-13 12:43:34 +0530

    Update Sample Images

--- RaspberryPi/BUZZ_LED.py
+++ RaspberryPi/BUZZ_LED.py
@@ -1,5 +1,5 @@
 # Libraries
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from time import sleep
 import sys
 
@@ -8,65 +8,65 @@ folder_path = "ML part"
 sys.path.append(folder_path)
 from integrated import Detection as d
 
-# # Disable warnings (optional)
-# GPIO.setwarnings(False)
+# Disable warnings (optional)
+GPIO.setwarnings(False)
 
-# # Select GPIO mode
-# GPIO.setmode(GPIO.BCM)
+# Select GPIO mode
+GPIO.setmode(GPIO.BCM)
 
-# # Set buzzer - pin 23 as output
-# buzzer = 23
-# GPIO.setup(buzzer, GPIO.OUT)
+# Set buzzer - pin 23 as output
+buzzer = 23
+GPIO.setup(buzzer, GPIO.OUT)
 
-# # set red,green and blue pins
-# redPin = 12
-# greenPin = 19
-# bluePin = 13
+# set red,green and blue pins
+redPin = 12
+greenPin = 19
+bluePin = 13
 
-# # set pins as outputs
-# GPIO.setup(redPin, GPIO.OUT)
-# GPIO.setup(greenPin, GPIO.OUT)
-# GPIO.setup(bluePin, GPIO.OUT)
+# set pins as outputs
+GPIO.setup(redPin, GPIO.OUT)
+GPIO.setup(greenPin, GPIO.OUT)
+GPIO.setup(bluePin, GPIO.OUT)
 
 
 # Define LED functions
 def turnOff():
     print("Turned off!")
-    # GPIO.output(redPin, GPIO.HIGH)
-    # GPIO.output(greenPin, GPIO.HIGH)
-    # GPIO.output(bluePin, GPIO.HIGH)
+    GPIO.output(redPin, GPIO.HIGH)
+    GPIO.output(greenPin, GPIO.HIGH)
+    GPIO.output(bluePin, GPIO.HIGH)
 
 
 def red():
     print("RED LED!")
-    # GPIO.output(redPin, GPIO.LOW)
-    # GPIO.output(greenPin, GPIO.HIGH)
-    # GPIO.output(bluePin, GPIO.HIGH)
+    GPIO.output(redPin, GPIO.LOW)
+    GPIO.output(greenPin, GPIO.HIGH)
+    GPIO.output(bluePin, GPIO.HIGH)
 
 
 def green():
     print("GREEN LED!")
-    # GPIO.output(redPin, GPIO.HIGH)
-    # GPIO.output(greenPin, GPIO.LOW)
-    # GPIO.output(bluePin, GPIO.HIGH)
+    GPIO.output(redPin, GPIO.HIGH)
+    GPIO.output(greenPin, GPIO.LOW)
+    GPIO.output(bluePin, GPIO.HIGH)
 
 
 def yellow():
     print("YELLOW LED!")
-    # GPIO.output(redPin, GPIO.LOW)
-    # GPIO.output(greenPin, GPIO.LOW)
-    # GPIO.output(bluePin, GPIO.HIGH)
+    GPIO.output(redPin, GPIO.LOW)
+    GPIO.output(greenPin, GPIO.LOW)
+    GPIO.output(bluePin, GPIO.HIGH)
 
 
 def accident_detected():
     # if accident is severe, fast buzzing and red flashing lights
     if class_id == 1:
         while True:
-            # GPIO.output(buzzer, GPIO.HIGH)
+            GPIO.output(buzzer, GPIO.HIGH)
             print("Beep")
             sleep(0.5)
 
-            # GPIO.output(buzzer, GPIO.LOW)
+            GPIO.output(buzzer, GPIO.LOW)
             print("No Beep")
             sleep(0.5)
 
@@ -78,11 +78,11 @@ def accident_detected():
     # if accident is moderate, relatively slower buzzing and yellow flashing lights
     elif class_id == 0:
         while True:
-            # GPIO.output(buzzer, GPIO.HIGH)
+            GPIO.output(buzzer, GPIO.HIGH)
             print("Beep")
             sleep(2)  # Delay in seconds
 
-            # GPIO.output(buzzer, GPIO.LOW)
+            GPIO.output(buzzer, GPIO.LOW)
             print("No Beep")
             sleep(2)
 
@@ -95,20 +95,17 @@ def accident_detected():
 result_list = []
 
 while True:
-    result_list = d.staticDetection()
-
-    class_id = result_list[0]
-    confidence_score = result_list[1]
-
-    print(f"Class ID: {class_id}")
-    print(f"Confidence Score: {confidence_score}")
-
-    # Run forever loop - to indicate the system is running
-    # as soon as accident is detected, buzzer is sound and appropriate colors on LED are shown
+    # Run forever loop with blinking green LED to indicate the system is running
     turnOff()
     sleep(1)  # 1second
     green()
     sleep(1)
+
+    # mapping class id and confidence score to returned list of values
+    [class_id, confidence_score] = d.staticDetection()
+
+    print(f"Class ID: {class_id}")
+    print(f"Confidence Score: {confidence_score}")
 
     if confidence_score >= 0.5:
         accident_detected()
