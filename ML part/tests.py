8cdd4a1f6621f08755e64a46b31f3b9baccf86f4
Gaurav Hegade <74713169+gauravhegade@users.noreply.github.com>
2023-09-13 18:07:59 +0530

    Update tests.py

--- ML part/tests.py
+++ ML part/tests.py
@@ -1,15 +1,14 @@
 from integrated import Detection as d
 
-result_list = []
+# uncomment whichever one you want to test, results will be displayed on the terminal
 
-# Static detection
-# result_list = d.staticDetection()
+# # Static detection
+# [class_id, confidence_score] = d.staticDetection()
 # print(class_id)
 # print(confidence_score)
 
-# Video stream detection
-while True:
-    result_list = d.videoStreamDetection()
-    print(result_list)
-    # print(class_id)
-    # print(confidence_score)
+# #Video stream detection
+# while True:
+#     [class_id, confidence_score] = d.videoStreamDetection()
+#     print(class_id)
+#     print(confidence_score)
