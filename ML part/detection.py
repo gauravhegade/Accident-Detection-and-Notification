f7a59b9fdd06d0fe3f2d437e29dce03c47650d9d
Gaurav Hegade <neph005@protonmail.com>
2023-11-16 13:31:17 +0530

    Add object detection functionality with email and WhatsApp notifications

--- ML part/detection.py
+++ ML part/detection.py
@@ -2,82 +2,91 @@ import cv2
 from ultralytics import YOLO
 import smtplib
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from twilio.rest import Client
+import env_vars as ev
 
-# Load a model
+# Load the trained YOLO model
 model = YOLO("ML part/best.pt")
 
 
 def send_whatsapp_message(to_whatsapp_number, message):
-    # Your Twilio Account SID and Auth Token
-    account_sid = ""
-    auth_token = ""
+    # Function to send WhatsApp message using Twilio API
+    account_sid = ev.account_sid
+    auth_token = ev.auth_token
+    from_whatsapp_number = ev.from_whatsapp_number
 
-    # Create a Twilio client
     client = Client(account_sid, auth_token)
 
-    # The number you purchased from Twilio or verified on Twilio
-    from_whatsapp_number = ""
-
     try:
-        # Send the WhatsApp message
+        # Send WhatsApp message
         message = client.messages.create(
             body=message,
             from_="whatsapp:" + from_whatsapp_number,
             to="whatsapp:" + to_whatsapp_number,
         )
         print("WhatsApp message sent successfully:", message.sid)
-        # true if whatsapp message sent
         return True
 
     except Exception as e:
         print("Error sending WhatsApp message:", str(e))
-        # false if any error
         return False
 
 
-def send_email_with_frame(frame_path):
-    msg = MIMEMultipart()
-    msg["From"] = "<from email here>"
-    msg["To"] = "<to email here>"
-    msg["Subject"] = "Suspicious Activity Detected"
+def send_email_with_frame(frame_path, class_name, rounded_conf, location):
+    try:
+        # Function to send email with an attached image
+        msg = MIMEMultipart()
+        msg["From"] = ev.from_email
+        msg["To"] = ev.to_email
+        msg["Subject"] = "Accident Detected"
 
-    # Attach the image
-    with open(frame_path, "rb") as img_file:
-        image = MIMEImage(img_file.read())
-        msg.attach(image)
+        # Create HTML content with formatting
+        body = f"""
+        <html>
+            <body>
+                <p>Accident detected of class <b>{class_name}</b> with severity level <b>{rounded_conf}%</b> at location <b>{location}</b>. Check the attached image.</p>
+            </body>
+        </html>
+        """
 
-    # Send email using SMTP
-    smtp_server = "smtp.gmail.com"
-    smtp_port = 587
-    smtp_username = ""
-    smtp_password = ""
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls()
-    server.login(smtp_username, smtp_password)
-    server.sendmail(
-        "<email here>", "<email here>", msg.as_string()
-    )
-    server.quit()
+        msg.attach(MIMEText(body, 'html'))
 
-    return True  # Return success flag
+        with open(frame_path, "rb") as img_file:
+            image = MIMEImage(img_file.read())
+            msg.attach(image)
+
+        # Send email using SMTP
+        smtp_server = "smtp.gmail.com"
+        smtp_port = 587
+        smtp_username = ev.smtp_username
+        smtp_password = ev.smtp_password
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        server.sendmail(ev.from_email, ev.to_email, msg.as_string())
+        server.quit()
+        
+        return True  # Return success flag
+
+    except Exception as e:
+        print("Error sending email:", str(e))
+        return False
 
 
-# detection
 class Detection:
     detection_result = []
 
+    @staticmethod
     def prediction(path):
-        # detection
+        # Function to perform object detection using YOLO model
         results = model.predict(source=path, show=False)
 
         for result in results:
             for box in result.boxes:
-                # extracting data to appropriate variables if an accident is detected means a box is created around that accident area coords
                 class_id = box.cls[0].item()
-                cords = box.xyxy[0].tolist()
-                cords = [round(x) for x in cords]
+                cords = [round(x) for x in box.xyxy[0].tolist()]
                 conf = round(box.conf[0].item(), 2)
 
                 if conf >= 0.5:  # Adjust the confidence threshold as needed
@@ -86,54 +95,48 @@ class Detection:
                     print("Confidence Score:", conf)
                     print("---")
 
+                    location = "*LOCATION*"
+                    class_name = "*MODERATE*" if class_id == 0 else "*SEVERE*"
+                    rounded_conf = str(round(conf * 100, 2))
+
                     # Send email notification
-                    if send_email_with_frame(path):
+                    if send_email_with_frame(path, class_name, rounded_conf, location):
                         print("Email sent!")
 
-                    location = "location"
-                    message = f"Accident detected at location {location}. Severity level: {conf*100}%. Check email for image."
-                    send_whatsapp_message("<your number here>", message)
+                    message = f"Accident detected of class { class_name } at location { location }. Severity level: { rounded_conf }%. Check email for image."
+                    send_whatsapp_message(ev.to_whatsapp_number, message)
 
-                    # reinitialize the result list to empty to get updated values for the next detection
+                    # Reinitialize the result list to empty to get updated values for the next detection
                     Detection.detection_result = []
                     Detection.detection_result.append(class_id)
                     Detection.detection_result.append(conf)
                     return Detection.detection_result
 
-        # return dummy list if nothing detected
+        # Return dummy list if nothing detected
         return [0, 0]
 
-    def staticDetection():
-        # path variables
-        image_path = "ML part/sample_input/images/image9.jpg"
-        # video_path = "ML part/sample_input/videos/video3.mp4"
-
+    @staticmethod
+    def static_detection():
+        # Function to perform static image detection
+        image_path = "ML part/inputs/images/image5.jpg"
         Detection.detection_result = Detection.prediction(image_path)
         return Detection.detection_result
 
-    def videoStreamDetection():
-        # Set the dimensions for captured frames
-        frame_width = 640
-        frame_height = 480
-
-        # known freely available urls with traffic stream:
-        # http://204.106.237.68:88/mjpg/1/video.mjpg
-        # URL of the video stream
+    @staticmethod
+    def video_stream_detection():
+        # Function to perform video stream detection
+        frame_width, frame_height = 1280, 720
         stream_url = "http://195.196.36.242/mjpg/video.mjpg"
 
-        # start capturing
         cap = cv2.VideoCapture(stream_url)
 
         while cap.isOpened():
-            # boolean success flag and the video frame
-            # if the video has ended, the success flag is False
+            # Boolean success flag and the video frame
             is_frame, frame = cap.read()
             if not is_frame:
                 break
 
             resized_frame = cv2.resize(frame, (frame_width, frame_height))
-
-            # Save the resized frame as an image in temporary directory
             temp_image_path = "ML part/temp.jpg"
             cv2.imwrite(temp_image_path, resized_frame)
 
