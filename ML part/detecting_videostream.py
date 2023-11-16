eb39f9cffe9fa7f92467ddd05a76d5938a84aa35
Gaurav Hegade <74713169+gauravhegade@users.noreply.github.com>
2023-11-16 11:33:41 +0530

    Update temp folder path and add comments

--- ML part/detecting_videostream.py
+++ ML part/detecting_videostream.py
@@ -11,7 +11,7 @@ frame_height = 480
 # known freely available urls with traffic stream:
 # http://204.106.237.68:88/mjpg/1/video.mjpg
 
-# URL of the video stream
+# URL of the video stream - preferably use motion jpg urls
 stream_url = "http://kamera.mikulov.cz:8888/mjpg/video.mjpg"
 
 # start capturing
@@ -26,16 +26,15 @@ while cap.isOpened():
 
     resized_frame = cv2.resize(frame, (frame_width, frame_height))
 
-    # Save the resized frame as an image in temporary directory
-    temp_image_path = "./temp_frame.jpg"
+    # Save the resized frame as an image in a temporary directory
+    temp_image_path = "ML part/temp/temp.jpg"
     cv2.imwrite(temp_image_path, resized_frame)
 
     # Perform object detection on the image and show the results
     model.predict(source=temp_image_path, show=True)
 
     # Check for the 'q' key press to quit
-    key = cv2.waitKey(1)
-    if key & 0xFF == ord("q") or key == 27:
+    if cv2.waitKey(1) == 0xff & ord('q'):
         break
 
 # Release the video capture and close any OpenCV windows
